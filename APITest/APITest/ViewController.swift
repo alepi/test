8969d7bc2817d19fb9efd6e745895a952c02e71c
alepi <667578+alepi@users.noreply.github.com>
2022-11-07 15:49:26 +0100

    initial commit

---
+++ APITest/APITest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  APITest
+//
+//  Created by yawmoo_ext on 07/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.view.backgroundColor = UIColor.red
+    }
+}
+
